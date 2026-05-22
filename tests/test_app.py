36c2282c0d89908ede0213768c81dee45ba86f7e
samawael7 <samawael764@gmail.com>
2026-05-22 03:34:04 +0300

    fixing pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,20 @@
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'app'))
 
-from app import app
+from app import app, init_db
 
 def test_index():
-    client = app.test_client()
-    res = client.get('/')
-    assert res.status_code == 200
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        with app.app_context():
+            init_db()
+        res = client.get('/')
+        assert res.status_code == 200
 
 def test_add_task():
-    client = app.test_client()
-    res = client.post('/add', data={'title': 'Test Task'}, follow_redirects=True)
-    assert res.status_code == 200
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        with app.app_context():
+            init_db()
+        res = client.post('/add', data={'title': 'Test Task'}, follow_redirects=True)
+        assert res.status_code == 200
